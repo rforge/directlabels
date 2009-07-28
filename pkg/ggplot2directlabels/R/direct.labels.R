5758e17150f74aa62b7beafaecade2f01baf9dc5
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-28 15:38:49 +0000

    ggplot2 with examples of text going off the page
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ggplot2directlabels/R/direct.labels.R
+++ pkg/ggplot2directlabels/R/direct.labels.R
@@ -1,9 +1,18 @@
 library(ggplot2)
 library(nlme)
 data(BodyWeight)
-ggplot(BodyWeight, aes(Time, weight, colour = Rat)) +
+p <- ggplot(BodyWeight, aes(Time, weight, colour = Rat)) +
   geom_line()  +
   geom_text(aes(label = Rat), subset = .(Time == min(Time)),hjust=1) +
   facet_grid(~ Diet) +
   ##theme_bw()+
   scale_colour_identity()
+pp <- function(w){
+  f <- paste(w,'png',sep='.')
+  png(f,w=w)
+  print(p)
+  dev.off()
+  system(paste("eog",f))
+}
+pp(500)
+pp(1000)
