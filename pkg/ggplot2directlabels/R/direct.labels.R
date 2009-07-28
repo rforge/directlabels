416f6837a102ee4650cad9bd8e0a9d33e4c3404a
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-28 15:27:41 +0000

    ggplot2 example working, need to rescale time so that numbers are not clipped
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ggplot2directlabels/R/direct.labels.R
+++ pkg/ggplot2directlabels/R/direct.labels.R
@@ -1,8 +1,9 @@
 library(ggplot2)
 library(nlme)
 data(BodyWeight)
-
-ggplot(BodyWeight, aes(Time, weight, colour = subject)) +
+ggplot(BodyWeight, aes(Time, weight, colour = Rat)) +
   geom_line()  +
-  geom_text(aes(label = subject), subset = .(Time == min(Time))) +
-  facet_wrap(~ treatment)
+  geom_text(aes(label = Rat), subset = .(Time == min(Time)),hjust=1) +
+  facet_grid(~ Diet) +
+  ##theme_bw()+
+  scale_colour_identity()
