29f56cc67f4b416f7fc59263bc5187922429ee4a
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-02 22:52:06 +0000

    png making code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/latticedirectlabels/tests/time.series.R
+++ pkg/latticedirectlabels/tests/time.series.R
@@ -1,5 +1,6 @@
 data(BodyWeight,package="nlme")
-print(dl(xyplot,BodyWeight,weight~Time|Diet,Rat,
-         type='l',layout=c(3,1)))
+pdfpng("longitudinal",dl(xyplot,BodyWeight,weight~Time|Diet,Rat,
+         type='l',layout=c(3,1)),h=7,w=14)
+
 ## Fails: default method for scatterplot doesn't make sense here
 ##print(dl(xyplot,BodyWeight,weight~Time|Diet,Rat))
