c8d19b4b66e2de07518cbcc707f54817c29dcdaa
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-02 22:02:50 +0000

    more tests for car data set
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/latticedirectlabels/tests/time.series.R
+++ pkg/latticedirectlabels/tests/time.series.R
@@ -2,4 +2,4 @@ data(BodyWeight,package="nlme")
 print(dl(xyplot,BodyWeight,weight~Time|Diet,Rat,
          type='l',layout=c(3,1)))
 ## Fails: default method for scatterplot doesn't make sense here
-print(dl(xyplot,BodyWeight,weight~Time|Diet,Rat))
+##print(dl(xyplot,BodyWeight,weight~Time|Diet,Rat))
--- pkg/latticedirectlabels/tests/xyplot.R
+++ pkg/latticedirectlabels/tests/xyplot.R
@@ -4,6 +4,14 @@ data(mpg)
 m <- lm(cty~displ,data=mpg)
 mpgf <- fortify(m,mpg)
 plot(dl(xyplot,mpgf,.resid~.fitted,factor(cyl),
-        panel=function(...){panel.abline(1);panel.xyplot(...)},main="foobar2",
+        panel=function(...){panel.abline(1);panel.xyplot(...)},
+        main="foobar2",
         method=parallel.lines))
 plot(dl(xyplot,mpgf,.resid~.fitted,factor(cyl),debug=TRUE))
+## Should fail: (default method includes parallel line calculation, which makes no sense for only 1 group per panel
+plot(dl(xyplot,mpgf,.resid~.fitted|cyl,factor(cyl)))
+## Should work, but not very informative:
+plot(dl(xyplot,mpgf,.resid~.fitted|cyl,factor(cyl),method=empty.grid))
+mpgf$cyl10 <- sapply(mpgf$cyl,function(i)paste(rep(i,l=10),collapse=""))
+plot(dl(xyplot,mpgf,.resid~.fitted|cyl,factor(cyl10),method=empty.grid))
+plot(dl(xyplot,mpgf,.resid~.fitted|manufacturer,factor(cyl),method=empty.grid.2))
