1df794103badd47c06ef7d9cb7ba5fb7abbafdb1
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-08-04 15:34:58 +0000

    no line break for dl in basic example
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -87,8 +87,7 @@ install.packages("latticedl", repos="http://R-Forge.R-project.org")
 library(latticedl)
 loci <- data.frame(ppp=c(rbeta(800,10,10),rbeta(100,0.15,1),rbeta(100,1,0.15)),
                    type=factor(c(rep("NEU",800),rep("POS",100),rep("BAL",100))))
-dl(densityplot,loci,~ppp,type,
-   n=500)
+dl(densityplot,loci,~ppp,type,n=500)
 </pre>
 <img src="density.png" />
 <p>
