4b8b4fe3717b5243fc01cf0c940188be735e45f9
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-09-28 13:42:26 +0000

    note c()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@63 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- talks/semin-r/test.R
+++ talks/semin-r/test.R
@@ -1,6 +1,9 @@
 data(VADeaths)
+print(VADeaths)
 library(lattice)
 library(latticedl)
 direct.label(
              dotplot(VADeaths,type="o")
              ,method=function(...)data.frame(last.points(...),rot=30))
+library(latticeExtra)
+c(l1,l2) #for combining lattice plots on the same page.
