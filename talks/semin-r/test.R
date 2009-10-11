17aeabd625f9f7c8812fdb3b4fe4660068bacad0
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-11 22:24:00 +0000

    more organized comments and demo code for talk
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- talks/semin-r/test.R
+++ talks/semin-r/test.R
@@ -31,44 +31,14 @@ densityplot(~gcsescore|factor(score),Chem97,groups=gender,auto.key=list(columns=
 show.settings()
 
 ## Show settings good for printout
-show.settings(col.whitebg())
+show.settings(standard.theme(color=FALSE))
 
 ## Change the settings
-rb <- simpleTheme(col=c("black","red"))
+br <- simpleTheme(col=c("black","red"))
 show.settings(rb)
 
 ## Change group colors with par.settings
-densityplot(~gcsescore|factor(score),Chem97,groups=gender,auto.key=list(columns=2,space="bottom"),par.settings=list(superpose.line=list(col=c("black","red"))))
-
-## Query graphical parameters
-names(trellis.par.get())
-
-## Query graphical parameters for grouped lineplots
-trellis.par.get("superpose.line")
-
-## Shorter specification using simpleTheme
-densityplot(~gcsescore|factor(score),Chem97,groups=gender,auto.key=list(columns=2,space="bottom"),par.settings=)
-
-## simple one-time positioning function:
-qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"))
-direct.label(qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"),f.value=ppoints(100)))
-direct.label(qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"),f.value=ppoints(100)),method=function(d,...)data.frame(x=c(-2,0),y=c(6,4),groups=c("M","F")))
-direct.label(qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"),f.value=ppoints(100)),method=data.frame(x=c(-2,0),y=c(6,4),groups=c("M","F")))
-direct.label(qqmath(~gcsescore|gender,Chem97,groups=factor(score),type=c('l','g'),f.value=ppoints(100)))
-
-qq(gender~gcsescore|factor(score),Chem97,type=c("p","g"),aspect=1)
-bwplot(factor(score)~gcsescore|gender,Chem97)
-bwplot(gcsescore~gender|factor(score),Chem97,layout=c(6,1))
-
-head(quakes)
-stripplot(depth~factor(mag),quakes,
-          main="Depth of earthquake epicenters by magnitude",
-          xlab="Magnitude (Richter)",
-          ylab="Depth (km)")
-xyplot(depth~factor(mag),quakes,
-          main="Depth of earthquake epicenters by magnitude",
-          xlab="Magnitude (Richter)",
-          ylab="Depth (km)")
+densityplot(~gcsescore|factor(score),Chem97,groups=gender,auto.key=list(columns=2,space="bottom"),par.settings=br)
 
 ## Load a tabular data set
 print(VADeaths)
@@ -94,23 +64,24 @@ dotplot(age~deaths|demographic,vad,layout=c(1,4))
 dotplot(age~deaths|demographic,vad,layout=c(1,4),type="o")
 
 ## Use grouping instead of conditioning to facilitate comparison
-dots <- dotplot(age~deaths,vad,groups=demographic,type="o")
+dotplot(age~deaths,vad,groups=demographic)
+
+## Plots can be saved as R objects
+dots <- dotplot(age~deaths,vad,groups=demographic)
 dots
 
-## Add a confusing legend ... how to label better?
-update(dots,auto.key=list())
+## Saved plots can be updated later
+dots2 <- update(dots,type="o")
+dots2
 
-## Direct label the different groups
-library(latticedl)
-direct.label(dots)
-direct.label(dots,method=function(...)data.frame(last.points(...),rot=30))
-direct.label(dots,method=list(rot=90,hjust=-0.1))
+## Add a confusing legend ... how to label better?
+update(dots2,auto.key=list())
 
 ## Load some earthquake measurements
 data(Earthquake,package="nlme")
 head(Earthquake)
 
-## Scatter plot with xyplot
+## Scatterplot with xyplot
 xyplot(accel~distance,Earthquake)
 
 ## Log scales with scales argument
@@ -119,33 +90,71 @@ xyplot(accel~distance,Earthquake,scales=list(log=TRUE))
 ## Type "p" is the default
 xyplot(accel~distance,Earthquake,scales=list(log=TRUE),type=c("p"))
 
-## Type
+## Type "g" adds a grid
 xyplot(accel~distance,Earthquake,scales=list(log=TRUE),type=c("p","g"))
-xyplot(accel~distance,Earthquake,scales=list(log=TRUE),
-       type=c("p","g","smooth"))
 
+## Type "smooth" adds a smooth line
+xyplot(accel~distance,Earthquake,scales=list(log=TRUE),type=c("p","g","smooth"))
 
+## Add some labels
+xyplot(accel~distance,Earthquake,scales=list(log=TRUE),type=c("p","g","smooth"),xlab="Distance from epicenter (km)",ylab="Maximum horizontal acceleration (g)",main="Larger quakes are felt closer to the epicenter",sub="Data on log scale")
+
+## Make some 3d data
 x <- seq(pi/4, 5 * pi, length.out = 100)
 y <- seq(pi/4, 5 * pi, length.out = 100)
 r <- as.vector(sqrt(outer(x^2, y^2, "+")))
 grid <- expand.grid(x=x, y=y)
 grid$z <- cos(r^2) * exp(-r/(pi^3))
 head(grid)
+
+## Plot 3 categorical variables using color
 levelplot(z~x*y,grid)
+
+## Add more levels to the color scale
 levelplot(z~x*y,grid,cuts=50)
+
+## Use a different color scale
 my.colors <- sapply(0:100,function(l)hcl(l=l))
 levelplot(z~x*y,grid,cuts=50,col.regions=my.colors)
 
-head(volcano)
-levelplot(volcano)
+## Data in matrix form of volcano heights
+dim(volcano)
+volcano[1:5,1:5]
+
+## Plot volcano elevations in a matrix using color
 levelplot(volcano,col.regions=my.colors)
-wireframe(volcano, shade = TRUE,
-          aspect = c(61/87, 0.4),
-          light.source = c(10,0,10))
 
+## Use 3d wireframe plots
+wireframe(volcano,drape=TRUE,col.regions=my.colors)
 
+## Combine plots using latticeExtra
+library(latticeExtra)
+trellis.par.set(regions=list(col=my.colors))
+c(wireframe(volcano,drape=TRUE),levelplot(volcano))
 
+## Box and whisker plots
+bwplot(gcsescore~gender|factor(score),Chem97,layout=c(6,1))
+
+## Compare the distribution (sample quantiles) of 2 variables
+qq(gender~gcsescore|factor(score),Chem97)
+
+## Force aspect ratio to be equal
+qq(gender~gcsescore|factor(score),Chem97,aspect=1)
+
+## Determine the normality of a variable
+qqmath(~gcsescore,Chem97,groups=gender,auto.key=list(space="right"))
+
+## Only plot 100 probability points (faster)
+qqmath(~gcsescore,Chem97,groups=gender,auto.key=list(space="right"),f.value=ppoints(100))
+
+## Use lines instead of points ... legend?
+qqmath(~gcsescore,Chem97,groups=gender,auto.key=list(space="right"),f.value=ppoints(100),type="l")
+
+## Easy fix for confusing legend: direct labels
 library(latticedl)
+direct.label(qqmath(~gcsescore,Chem97,groups=gender,f.value=ppoints(100),type="l"))
+
+
 direct.label(densityplot(~gcsescore,Chem97,groups=gender))
 
 library(lattice)
@@ -161,3 +170,12 @@ direct.label(qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"),f.value=ppoi
 direct.label(qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"),f.value=ppoints(100)),method=function(d,...)data.frame(x=c(-2,0),y=c(6,4),groups=c("M","F")))
 direct.label(qqmath(~gcsescore,Chem97,groups=gender,type=c("p","g"),f.value=ppoints(100)),method=data.frame(x=c(-2,0),y=c(6,4),groups=c("M","F")))
 direct.label(qqmath(~gcsescore|gender,Chem97,groups=factor(score),type=c('l','g'),f.value=ppoints(100)))
+## Direct label the different groups
+## Add a confusing legend ... how to label better?
+update(dots2,auto.key=list())
+
+library(latticedl)
+direct.label(dots)
+direct.label(dots,method=function(...)data.frame(last.points(...),rot=30))
+direct.label(dots,method=list(rot=90,hjust=-0.1))
+
