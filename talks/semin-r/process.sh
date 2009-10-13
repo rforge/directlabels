4d3689a191718442f64c2dcbf58871bbca9a8eb0
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-13 06:39:20 +0000

    remove references to old convert.py
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- talks/semin-r/process.sh
+++ talks/semin-r/process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python convert.py HOCKING-latticedl-semin-r-in.Rnw > HOCKING-latticedl-semin-r.Rnw && \
+python make_sweave.py HOCKING-latticedl-semin-r-in.Rnw > HOCKING-latticedl-semin-r.Rnw && \
  R CMD Sweave HOCKING-latticedl-semin-r.Rnw && \
  python killplot.py HOCKING-latticedl-semin-r.tex && \
  cp HOCKING-latticedl-semin-r.tex.new HOCKING-latticedl-semin-r.tex && \
