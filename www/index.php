fb79ff015c07961edc3813dd045dd0e5e2c0a035
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-14 16:09:09 +0000

    explain labelling method lists
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@110 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -82,8 +82,7 @@ of useful functions that make direct labeling of common plots easy to
 do. The first working examples are based on lattice graphics:</p>
 
 <pre>
-install.packages("ggplot2") # for dependencies on CRAN
-install.packages("latticedl", repos="http://R-Forge.R-project.org")
+install.packages("latticedl")
 library(latticedl)
 loci <- data.frame(ppp=c(rbeta(800,10,10),rbeta(100,0.15,1),rbeta(100,1,0.15)),
                    type=factor(c(rep("NEU",800),rep("POS",100),rep("BAL",100))))
@@ -111,7 +110,7 @@ direct.label(
 </pre>
 
 <p>For direct labeling plots we always will specify the groups=
-argument. Thus to save a bit of typing the dl() shortcut function is
+argument. Thus to save a bit of typing the <tt>dl()</tt> shortcut function is
 provided:</p>
 
 <pre>
@@ -128,26 +127,17 @@ the arguments involving data variables.</p>
 <h2>An extensible framework for thinking about direct labeling
 problems</h2>
 
-<p> Direct labeling a plot basically consists of 2 steps:
-</p>
+<p>Direct labeling a plot can be decomposed into 2 steps: calculating
+label positions, then drawing the labels. Drawing the labels will
+always be taken care of for you, using the color of the corresponding
+group. Calculating label positions is also done for you for common
+plot types. For example, with the density plot above, we placed the
+each label above the mode of the corresponding density estimate.</p>
 
-<ul> 
-
-<li><b>Label positioning:</b> calculating where you want to put the
-labels based on the data points. This is done by the Positioning
-Function, specified by the method= argument to dl.</li>
-
-<li><b>Drawing the labels:</b> we will always take care of drawing the
-labels for you, using the right color.</li>
-
-</ul>
-
-<p>Label positioning is something that changes from graphic to
-graphic. Thus we introduce the concept of a Positioning
-Function. Change the Positioning Function, and you change the position
-of your direct labels. For example, we can label longitudinal data
-either on the left or right of the lines, using included Positioning
-Functions as follows:</p>
+<p>If default label positions are not satisfactory, you can always
+specify your own label placement method, using the method= argument to
+direct.label. For example, we can label longitudinal data either on
+the left or right of the lines:</p>
 
 <pre>
 library(latticedl)
@@ -156,22 +146,46 @@ p <- xyplot(weight~Time|Diet,BodyWeight,groups=Rat,type="l",layout=c(3,1))
 direct.label(p,method=first.points)
 direct.label(p,method=last.points)
 </pre>
-<img src="compare-long.png" />
+<img src="compare-long.png" alt="direct label longitudinal data" />
 
-<p>The package comes with several built-in positioning functions, and
-tries to choose the best one to use based on your choice of high-level
-lattice function. The defaults will not be right for every one of your
-graphs, but you can always make the labels appear at the right place
-by writing your own Positioning Functions tailored to your data. If
-you want to position your labels yourself, write a Positioning
-Function that takes the input data points as a data frame with columns
-<tt>x y groups</tt>, and returns a data frame with columns <tt>x y
-groups</tt> that describes where the labels should be positioned for
-each group.</p>
+<p>Here first.points and last.points are Positioning Functions of the
+form function(d,...){return(data.frame(x=,y=,groups=))}, where d is
+all the data to plot, as a data frame with columns <tt>x y
+groups</tt>. first.points simply returns the rows of the data frame
+which correspond to the first points for each group. latticedl plots a
+direct label for each row returned by the positioning method.</p>
+
+<p>Since the input and output of a Positioning Functions is the same
+sort of data frame, you can chain Positioning Functions together. To
+illustrate how this works, consider the following contrived
+example:</p>
+
+<pre>
+complicated <- list(function(d,...){d$x <- d$x+10;d}, ## add 10 to every x value
+                    dl.indep(d[-2,]), ## delete the 2nd point of every group
+                    rot=c(30,180)) ## rotate by alternately 30 and 180 degrees
+direct.label(dotplot(VADeaths,type="o"),method=complicated)
+</pre>
+
+<img src="method2.png" alt="contrived direct labels" />
+
+<p>When method is specified as a list, the arguments will be applied
+in sequence, starting from the data frame of all plotted points. In
+this example the first element is a function that adds 10 to every x
+value, thus shifting the labels to the right relative to the
+points. The second element is a call to dl.indep, which lets you
+specify an expression to apply to each group, as a function of the
+data d. In this case d[-2,] means delete the second row from each
+group. Named elements are copied to the data frame, so the third
+element, rot=c(30,180), adds a rot column to the data frame, with
+values 30 and 180 for every other row. This has the effect of rotating
+every label by 30 or 180 degrees. Text display parameters fontsize,
+fontfamily, fontface, lineheight, and cex can also be specified in
+this manner (see the help page for grid::grid.text).</p>
 
 <p>Also note that the drawing functions are totally linked to the
-lattice graphics framework, but we can use Positioning Functions with
-other plotting frameworks, i.e. ggplot2.</p>
+lattice graphics framework, but we can potentially use Positioning
+Functions with other plotting frameworks, i.e. ggplot2.</p>
 
 <h2>Talks</h2>
 
