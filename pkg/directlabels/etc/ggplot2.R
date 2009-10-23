d7cadbc3996a272ae3c9786242e4d23c5ff32465
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-23 14:44:39 +0000

    Working for 3 ggplot2 examples, no defaults yet.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@131 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/etc/ggplot2.R
+++ pkg/directlabels/etc/ggplot2.R
@@ -1,4 +1,13 @@
 library(ggplot2)
+
+
+loci <- data.frame(ppp=c(rbeta(800,10,10),rbeta(100,0.15,1),rbeta(100,1,0.15)),
+                   type=factor(c(rep("NEU",800),rep("POS",100),rep("BAL",100))))
+direct.label(
+             qplot(ppp,data=loci,colour=type,geom="density")
+             ,method=list(trans.densityplot,top.points))
+
+
 library(nlme)
 data(BodyWeight)
 p <- ggplot(BodyWeight, aes(Time, weight, colour = Rat)) +
@@ -7,20 +16,19 @@ p <- ggplot(BodyWeight, aes(Time, weight, colour = Rat)) +
   facet_grid(~ Diet) +
   ##theme_bw()+
   scale_colour_identity()
-pp <- function(w){
-  f <- paste(w,'png',sep='.')
-  png(f,w=w)
-  print(p)
-  dev.off()
-  system(paste("eog",f))
-}
-pp(500)
-pp(1000)
 pdl <- direct.label(p,last.points)
 pdl
+direct.label(qplot(Time,weight,data=BodyWeight,colour=Rat,geom="line",facets=.~Diet),last.points)+scale_colour_identity()
+## error, ok since no colour specified:
+direct.label(qplot(Time,weight,data=BodyWeight,group=Rat,geom="line",facets=.~Diet),last.points)+scale_colour_identity()
+
+
 
 vad <- as.data.frame.table(VADeaths)
 names(vad) <- c("age","demographic","deaths")
 p2 <- qplot(deaths,age,data=vad,group=demographic,geom="line",colour=demographic)
-p3 <- direct.label(p2,list(last.points,rot=30))
+p2
+p3 <- direct.label(p2,list(last.points,rot=30),TRUE)
 p3
+p3+scale_colour_identity()## error, how do we hide the legend here?
+
