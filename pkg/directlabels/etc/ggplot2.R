34a57309318c5258b2f4237d80ab29c581a62f4a
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-10-28 13:01:04 +0000

    drop scale_colour_identity, do more comparisons
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@146 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/etc/ggplot2.R
+++ pkg/directlabels/etc/ggplot2.R
@@ -5,7 +5,6 @@ loci <- data.frame(ppp=c(rbeta(800,10,10),rbeta(100,0.15,1),rbeta(100,1,0.15)),
                    type=factor(c(rep("NEU",800),rep("POS",100),rep("BAL",100))))
 dp <- qplot(ppp,data=loci,colour=type,geom="density")
 dp
-dp+scale_colour_identity()
 pdf("densityplot-ggplot2.pdf",width=5,height=5)
 direct.label(dp,method=list(trans.densityplot,top.points))
 dev.off()
@@ -23,20 +22,20 @@ pdl <- direct.label(p,last.points)
 pdl
 qpdl <- qplot(Time,weight,data=BodyWeight,colour=Rat,geom="line",facets=.~Diet)
 qpdl
-qpdl+scale_colour_identity()
-direct.label(qplot(Time,weight,data=BodyWeight,colour=Rat,geom="line",facets=.~Diet),last.points)+scale_colour_identity()
+direct.label(qplot(Time,weight,data=BodyWeight,colour=Rat,geom="line",facets=.~Diet),last.points)
 ## error, ok since no colour specified:
-direct.label(qplot(Time,weight,data=BodyWeight,group=Rat,geom="line",facets=.~Diet),last.points)+scale_colour_identity()
+direct.label(qplot(Time,weight,data=BodyWeight,group=Rat,geom="line",facets=.~Diet),last.points)
 
 
 library(ggplot2)
 vad <- as.data.frame.table(VADeaths)
 names(vad) <- c("age","demographic","deaths")
-p2 <- qplot(deaths,age,data=vad,group=demographic,geom="line",colour=demographic)
+p2 <- qplot(deaths,age,data=vad,group=demographic,geom="line",colour=demographic)+xlim(5,80)
 p2
-direct.label(p2,list(last.points,rot=30),TRUE)+xlim(5,80)
+direct.label(p2,list(last.points,rot=30),TRUE)
 ## Compare with manual addition of direct labels:
 p2+geom_text(aes(label=demographic),subset=.(age==levels(age)[nlevels(age)]),hjust=0,angle=30)+opts(legend.position="none")
 direct.label(p2,function(d,...)data.frame(subset(d,y==max(y)),hjust=0,rot=30))
 direct.label(p2,list(dl.indep(d[which.max(d$x),]),hjust=0,rot=30))
 direct.label(p2,list(last.points,rot=30))
+ggsave("ex.pdf");system("xpdf ex.pdf")
