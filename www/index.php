2cc4f9754f97edb195d428e99b5468ffc933a0ea
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-18 09:38:26 +0000

    link type net -> org
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@182 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -82,7 +82,7 @@ functions that make direct labeling of common plots easy to do with
 high-level plotting systems such as lattice and ggplot2:</p>
 
 <pre>
-install.packages("directlabels",repos="http://r-forge.r-project.net")
+install.packages("directlabels",repos="http://r-forge.r-project.org")
 library(directlabels)
 loci <- data.frame(ppp=c(rbeta(800,10,10),rbeta(100,0.15,1),rbeta(100,1,0.15)),
                    type=factor(c(rep("NEU",800),rep("POS",100),rep("BAL",100))))
