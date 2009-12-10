1491bde9702663f84f3c4d697dd228e271da6819
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-10 18:16:45 +0000

    first html generation working
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@226 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/R/doc.R
+++ pkg/directlabels/R/doc.R
@@ -54,16 +54,59 @@ makehtml <- function # Make HTML docs
 (L
 ### List of positioning functions and plots to match up.
  ){
-  wwwdocs <- file.path("..","..","www","docs",L$type)
-  dir.create(wwwdocs,recursive=TRUE)
+  ## all paths are relative to the docs directory
+  odir <- setwd(file.path("..","..","www","docs"))
+  on.exit(setwd(odir))
+  subdir <- L$type
+  pngurls <- matrix("",nrow=length(L$posfuns),ncol=length(L$plots),
+                 dimnames=list(names(L$posfuns),
+                   sapply(L$plots,function(x)x$title)))
+  ## first make plots
+  tomake <- file.path(subdir,c("","plots","methods"))
+  for(d in tomake)
+    if(!file.exists(d))dir.create(d,recursive=TRUE)
   for(p in L$plots){
     cat(p$title,":",sep="")
     for(f in L$posfuns){
       cat(" ",f$name,sep="")
-      png(file.path(wwwdocs,paste(p$title,f$name,"png",sep=".")))
-      print(direct.label(p$plot,f$fun))
-      dev.off()
+      pngfile <- file.path(subdir,paste(p$title,f$name,"png",sep="."))
+      pngurls[f$name,p$title] <- pngfile
+      ##png(pngfile)
+      ##print(direct.label(p$plot,f$fun))
+      ##dev.off()
     }
     cat("\n")
   }
+  foot <- paste(readLines("templates/foot.html"),collapse="\n")
+  ## now make html for plot examples
+  for(p in L$plots){
+    tmp <- lapply(L$posfuns,function(f)
+                  c(f,url=file.path("..","..",pngurls[f$name,p$title])))
+    rowhtml <- sapply(tmp,filltemplate,"templates/posfun-row.html")
+    p$table <- paste(rowhtml,collapse="\n")
+    p$type <- L$type
+    p$pagetitle <- p$title
+    p$head <- filltemplate(p,"templates/head.html")
+    p$foot <- foot
+    html <- filltemplate(p,"templates/plot.html")
+    write(html,file.path(subdir,"plots",paste(p$title,".html",sep="")))
+  }
+}
+filltemplate <- function
+### Fill in occurances of OBJ$item in the file template with the value
+### in R of L$item.
+(L,template){
+  txt <- paste(readLines(template),collapse="")
+  L <- L[sapply(L,class)=="character"]
+  locs <- gregexpr("OBJ[$]([a-z]+)\\b",txt)[[1]]
+  keywords <- sapply(seq_along(locs),function(i)
+                     substr(txt,locs[i]+4,locs[i]+
+                            attr(locs,"match.length")[i]-1))
+  FIND <- sapply(keywords,function(x)paste("OBJ[$]",x,sep=""))
+  REP <- unlist(ifelse(keywords%in%names(L),L[keywords],""))
+  for(i in seq_along(FIND)){
+    txt <- gsub(FIND[i],REP[i],txt)
+  }
+  cat(txt,'\n\n')
+  txt
 }
