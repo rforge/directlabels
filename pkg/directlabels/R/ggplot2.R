aa336c3c3a99ff9529d7053b7a31a5b03267ff11
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-03 19:35:58 +0000

    contour testing
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@260 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/R/ggplot2.R
+++ pkg/directlabels/R/ggplot2.R
@@ -10,22 +10,27 @@ direct.label.ggplot <- function
 ### Show debug output?
  ){
   ##lvar <- if("group" %in% names(p$mapping))"group" else "colour"
-  varnames <- c(groups="colour",x="x")
-  if("y" %in% names(p$mapping))varnames <- c(varnames,y="y")
-  rename.vec <- sapply(p$mapping[varnames],deparse)
-  rename.vec <- gsub("[a-z]+[(]([^)]+)[)]","\\1",rename.vec)
-  d <- structure(p$data[,rename.vec],names=names(varnames))
-  geom <- p$layers[[1]]$geom$objname
-  ldefault <- if(nlevels(d$groups)==2)"lines2" else "maxvar.points"
-  if(is.null(method))method <-
-    switch(geom,
-           density="top.points",
-           line=ldefault,
-           point="empty.grid.2",
-           stop("No default label placement for this type of ggplot."))
-  if(geom%in%need.trans.ggplot)method <-
-    c(paste("trans.",geom,sep=""),method)
-  dlgeom <- geom_text(position=position_dl(list(method),debug))
+  if(missing(method)){
+    varnames <- c(groups="colour",x="x")
+    if("y" %in% names(p$mapping))varnames <- c(varnames,y="y")
+    rename.vec <- sapply(p$mapping[varnames],deparse)
+    rename.vec <- gsub("[a-z]+[(]([^)]+)[)]","\\1",rename.vec)
+    d <- structure(p$data[,rename.vec],names=names(varnames))
+    geom <- p$layers[[1]]$geom$objname
+    ldefault <- if(nlevels(d$groups)==2)"lines2" else "maxvar.points"
+    if(is.null(method))method <-
+      switch(geom,
+             density="top.points",
+             line=ldefault,
+             point="empty.grid.2",
+             stop("No default label placement for this type of ggplot."))
+    if(geom%in%need.trans.ggplot)method <-
+      c(paste("trans.",geom,sep=""),method)
+  }
+  ##print(p$layers[[1]]$mapping)
+  dlgeom <- geom_text(position=position_dl(list(method),debug,p),
+                      stat=p$layers[[1]]$stat)
+  ##browser()
   ##print(dlgeom)
   p+dlgeom+scale_colour_discrete(legend=FALSE)
 ### The ggplot object with direct labels added.
@@ -34,14 +39,21 @@ direct.label.ggplot <- function
 PositionDl <- proto(ggplot2::Position,{
   method <- NULL
   debug <- FALSE
-  new <- function(., method=NULL, debug=FALSE) {
-    .$proto(method=method,debug=debug)
+  orig <- NULL
+  new <- function(., method=NULL, debug=FALSE, orig=NULL) {
+    .$proto(method=method,debug=debug,orig=orig)
   }
   adjust <- function(.,data,scales){
-    ##print(data)
+    ##print(head(data))
     ##browser()
-    if(is.null(data$colour))stop("Need colour aesthetic to direct label.")
-    labtab <- label.positions(x=data$x,y=data$y,groups=data$colour,
+    if(is.null(data$colour)){
+      colvar <- .$orig$layers[[1]]$mapping$colour
+      if(!is.null(colvar)){
+        colvar <- gsub("^[.][.](.*)[.][.]$","\\1",colvar)
+        data$colour <- data[,colvar]
+      }else stop("Need colour aesthetic to direct label.")
+    }
+    labtab <- label.positions(x=data$x,y=data$y,groups=factor(data$colour),
                               subscripts=1:nrow(data),method=.$method[[1]],
                               debug=.$debug)
     targs <- list(label="groups",
@@ -55,7 +67,8 @@ PositionDl <- proto(ggplot2::Position,{
     targs <- targs[targs%in%names(labtab)]
     targs <- sapply(targs,as.name)
     r <- do.call("transform",c(list(labtab),targs))
-    ##print(r)
+    ##browser()
+    ##print(head(r))
     r
   }
   objname <- "dl"
---
+++ pkg/directlabels/etc/contour.R
@@ -0,0 +1,14 @@
+volcano3d <- melt(volcano)
+names(volcano3d) <- c("x", "y", "z")
+v <- ggplot(volcano3d, aes(x, y, z = z))
+v2 <- v + stat_contour(aes(colour = ..level..))
+v2+geom_text(position=position_dl(list(top.points),FALSE,v2))
+v2+geom_text(position=position_dl(list(top.points),FALSE,v2),stat="contour")
+v2+geom_text(position=position_dl(list(top.points),FALSE,v2),stat="contour")+
+  scale_colour_discrete(legend=FALSE)
+
+##positive control
+data(mpg,package="ggplot2")
+scatter <- qplot(jitter(hwy),jitter(cty),data=mpg,colour=class,
+                 main="Fuel efficiency depends on car size")
+direct.label(scatter,list(extreme.grid,dl.move("suv",15,15)))
