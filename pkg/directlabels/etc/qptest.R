ae332e1624eabb8959ea67b7f161ed888054ee25
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-02-13 19:00:11 +0000

    solvers give solutions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/etc/qptest.R
+++ pkg/directlabels/etc/qptest.R
@@ -21,6 +21,8 @@ d <- rep(0,2*n+1)
 d[1] <- m
 D <- diag(rep(0,2*n+1))
 D[1,1] <- 1
+epsilon <- 0.001
+diag(D) <- diag(D)+epsilon
 sol <- solve.QP(D,d,A,b0,n)
 
 ##simpler, only 1 point:
@@ -29,12 +31,14 @@ y <- 1
 f <- function(x)(x-m)^2
 curve(f(x),0,2)
 h <- 0.5
-pts <- c(y-0.5,y,y+0.5,m)
+pts <- c(y-h/2,y,y+h/2,m)
 pch <- c("[","y","]","m")
 points(pts,f(pts),pch=pch)
 A <- cbind(diag(rep(1,3)),c(0,1,1))
 b0 <- c(y,0,0,h/2)
 Q <- t(c(1,1,-1))
 D <- t(Q)%*%Q
+diag(D) <- diag(D)+epsilon
 d <- m*Q
-solve.QP(D,d,A,b0,1)
+sol <- solve.QP(D,d,A,b0,1)
+sol
