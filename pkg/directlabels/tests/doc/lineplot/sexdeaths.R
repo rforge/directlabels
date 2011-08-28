73e402fbc0afbc49fbe240e03ab90f5c6d1ef972
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-28 16:35:35 +0000

    delete p <-
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@506 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/tests/doc/lineplot/sexdeaths.R
+++ pkg/directlabels/tests/doc/lineplot/sexdeaths.R
@@ -3,4 +3,4 @@ tx <- time(mdeaths)
 Time <- ISOdate(floor(tx),round(tx%%1 * 12)+1,1,0,0,0)
 uk.lung <- rbind(data.frame(Time,sex="male",deaths=as.integer(mdeaths)),
                  data.frame(Time,sex="female",deaths=as.integer(fdeaths)))
-p <- qplot(Time,deaths,data=uk.lung,colour=sex,geom="line")
+qplot(Time,deaths,data=uk.lung,colour=sex,geom="line")
