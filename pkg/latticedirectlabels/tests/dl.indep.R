f5e664f4e756de8c7d60c4845603427dcf2cc700
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-01 22:45:44 +0000

    delete old pkgs to save space in working copy
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@538 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/latticedirectlabels/tests/dl.indep.R
+++
@@ -1,7 +0,0 @@
-library(latticedl)
-complicated <- list(dl.trans(x=x+10),
-                    dl.indep(d[-2,]),
-                    rot=c(30,180))
-direct.label(
-             dotplot(VADeaths,type="o")
-             ,method=complicated)
