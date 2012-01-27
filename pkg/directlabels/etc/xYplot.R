c548b167cbc80983296fa646738e1a0e80171e4c
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-27 08:14:04 +0000

    xYplot data test
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@560 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/etc/xYplot.R
+++ pkg/directlabels/etc/xYplot.R
@@ -8,6 +8,18 @@ dfr <- upData(dfr,
               upper=y + runif(48,.05,.15))
 p <- xYplot(Cbind(y,lower,upper) ~ month,groups=sex, type='b', data=dfr,
             subset=continent=='Europe',method='bands')
+print(p)
 library(directlabels)
 p2 <- direct.label(p)
 print(p2)
+
+
+library(Hmisc)
+library(directlabels)
+d <- read.table("xYplot.txt", header=T)
+p <- xYplot(items.taken ~ bll, groups=ncond,
+            data=subset(d, communicating=="T"),
+            pch=".",  ylim=c(0,8), method=smean.cl.normal, lty.bands=c(0,0,0,1))
+print(p)
+direct.label(p)
+
