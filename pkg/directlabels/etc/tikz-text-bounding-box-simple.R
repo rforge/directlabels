bf6ac0c9982d75e2c4515859ba1baf801fb07f41
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-27 17:52:56 +0000

    magic number for hat
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@613 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/etc/tikz-text-bounding-box-simple.R
+++ pkg/directlabels/etc/tikz-text-bounding-box-simple.R
@@ -6,7 +6,8 @@ s <- "$\\hat\\lambda$"
 grid.newpage()
 grid.text(s,0,0,hjust=0,vjust=0)
 x <- stringWidth(s)
-y <- stringHeight(s)
+## the magic number 1.4 gets the hat in the box.
+y <- stringHeight(s) * 1.4
 grid.rect(0,0,x,y,hjust=0,vjust=0)
 
 dev.off()
