ebd3e92bab290903472b667e55213deed49ecef3
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-06 17:37:50 +0000

    top.qp xlimits
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/directlabels@629 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/directlabels/R/dotplot.R
+++ pkg/directlabels/R/dotplot.R
@@ -3,5 +3,7 @@ angled.endpoints <- list("last.points",rot=30)
 
 ### Label points at the top, making sure they don't collide.
 top.qp <- list("top.points","calc.boxes",
-               qp.labels("x","left","right",make.tiebreaker("y","x")))
+               qp.labels("x","left","right",
+                         make.tiebreaker("y","x"),
+                         xlimits))
 
